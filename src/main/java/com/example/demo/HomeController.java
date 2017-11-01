0378417666523cc18f16aa3bb0d548909b03f028
GBTC408012UR <GBTC408012UR@MC.MONTGOMERYCOLLEGE.EDU>
2017-11-01 19:18:59 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/loadform")
+    public String loadFormPage() {
+
+        return "formtemplate";
+    }
+
+    @RequestMapping("/processform")
+    public String loadFromPage(@RequestParam("login") String Rlogin, Model model)
+    {
+        model.addAttribute("loginval", Rlogin);
+        return "confirm";
+
+
+    }
+
+}
+
